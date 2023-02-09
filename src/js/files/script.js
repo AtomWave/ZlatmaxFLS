9b11e98b3f3c066cd7d0da67efbc21a9b213c171
AtomWave <AtomWave@yandex.ru>
2023-02-09 19:31:06 +0300

    6:06

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -19,7 +19,6 @@ function documentActions(e) {
 	if (targetElement.closest('[data-parent]')) {
 		const subMenuId = targetElement.dataset.parent ? targetElement.dataset.parent : null;
 		const subMenu = document.querySelector(`[data-submenu="${subMenuId}"]`);
-		const catalogMenu = document.querySelector('.catalog-header');
 		if (subMenu) {
 			const activeLink = document.querySelector('._sub-menu-active');
 			const activeBlock = document.querySelector('._sub-menu-open');
@@ -37,6 +36,12 @@ function documentActions(e) {
 	}
 	if (targetElement.closest('.menu-top-header__link_catalog')){
 		document.documentElement.classList.add('catalog-open');
+		e.preventDefault();
+	}
+	if (targetElement.closest('.menu-catalog__back')) {
+		document.documentElement.classList.remove('catalog-open');
+		document.querySelector('._sub-menu-active') ? document.querySelector('._sub-menu-active').classList.remove('_sub-menu-active') : null;
+		document.querySelector('._sub-menu-open') ? document.querySelector('._sub-menu-open').classList.remove('_sub-menu-open') : null;
 		e.preventDefault();
 	}
 }
